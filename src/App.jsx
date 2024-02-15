5b84765e3917dc6a06538c21fd227ed049fff716
Vít Hrtus <vit.hrtus@gmail.com>
2024-02-15 18:56:01 +0100

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   const [cars, setCars] = useState(rawData.cars);
   const [carsToShow, setCarsToShow] = useState(rawData.cars);
   const [newCar, setNewCar] = useState({
-    id: cars.lenght > 0 ? Math.max(...cars.map((car) => car.id)) + 1 : 1,
+    id: cars.length > 0 ? Math.max(...cars.map((car) => car.id)) + 1 : 1,
     brand: "",
     model: "",
     reg: "",
@@ -31,36 +31,14 @@ function App() {
   const handleNewData = (data, source) => {
     switch (source) {
       case "add-car-form": {
-        const temp = [...cars];
-        temp.push(data);
-        setCars(temp);
-        setCarsToShow(temp);
-        setNewCar({
-          id: cars.lenght > 0 ? Math.max(...cars.map((car) => car.id)) + 1 : 1,
-          brand: "",
-          model: "",
-          reg: "",
-          km: "",
-          year: ""
-        });
+        setNewCar(data);
         break;
       }
       case "change-car-form": {
-        const index = cars.findIndex(car => car.id === data.id);
-        const updatedCars = [...cars];
-        updatedCars[index] = data; 
-        setCars(updatedCars);
-        setCarsToShow(updatedCars);
-        setCarToChange({
-          id: 0,
-          brand: "",
-          model: "",
-          reg: "",
-          km: "",
-          year: ""
-        });
+        setCarToChange(data);
         break;
       }
+      default: break;
     }
   }
 
@@ -69,19 +47,92 @@ function App() {
     setCars(temp);
     setCarsToShow(temp);
   }
+
   const handleChange = (idToChange) => {
     const temp = cars.filter(car => car.id === idToChange)
     setCarToChange(...temp);
   }
 
-  useEffect(() => { console.log(cars) }, [cars])
+  const fillEmptyInfos = (car) => {
+    const filledCar = {
+      ...car,
+      brand: car.brand.trim() ? car.brand : "empty",
+      model: car.model.trim() ? car.model : "empty",
+      reg: car.reg.trim() ? car.reg : "empty",
+      km: car.km.toString().trim() ? parseInt(car.km) : 0,
+      year: car.year.toString().trim() ? parseInt(car.year) : 0,
+    };
+    return filledCar;
+  }
 
+  const confirm = (car) => {
+    return (
+      window.confirm("Opravdu chcete odeslat data?\n" +
+        `Značka: ${car.brand}\n` +
+        `Model: ${car.model}\n` +
+        `Registrační značka: ${car.reg}\n` +
+        `Kilometry: ${car.km}\n` +
+        `Rok výroby: ${car.year}`)
+    );
+  }
+
+  const handleClick = (source) => {
+    let temp;
+    switch (source) {
+      case "add-car-form": {
+        temp = fillEmptyInfos(newCar);
+        if (confirm(temp)) {
+          const carsToUpdate = [...cars];
+          carsToUpdate.push(temp);
+          setCars(carsToUpdate);
+          setCarsToShow(carsToUpdate);
+          setNewCar({
+            id: temp.id + 1,
+            brand: "",
+            model: "",
+            reg: "",
+            km: "",
+            year: ""
+          });
+          alert("Data byla úspěšně odeslána.");
+        } else {
+          alert("Odeslání dat bylo zrušeno.");
+        }
+        break;
+      }
+      case "change-car-form": {
+        temp = fillEmptyInfos(carToChange);
+        if (confirm(temp)) {
+          const index = cars.findIndex(car => car.id === temp.id);
+          const carsToUpdate = [...cars];
+          carsToUpdate[index] = temp;
+          setCars(carsToUpdate);
+          setCarsToShow(carsToUpdate);
+          setCarToChange({
+            id: 0,
+            brand: "",
+            model: "",
+            reg: "",
+            km: "",
+            year: ""
+          });
+          alert("Data byla úspěšně odeslána.");
+        } else {
+          alert("Odeslání dat bylo zrušeno.");
+        }
+        break;
+      }
+      default: break;
+    }
+  }
+
+useEffect(() => { console.log(cars) }, [cars])
   return (
     <div className="container">
       <FilterForm data={cars} handleFilterData={handleFilterData} />
-      <UniForm id="add-car-form" data={newCar} handleNewData={handleNewData} />
+      <UniForm id="add-car-form" data={newCar} handleNewData={handleNewData} handleClick={handleClick} />
       <CarTable data={carsToShow} handleDelete={handleDelete} handleChange={handleChange} />
-      <UniForm id="change-car-form" data={carToChange} handleNewData={handleNewData} />
+      <UniForm id="change-car-form" data={carToChange} handleNewData={handleNewData} handleClick={handleClick} />
     </div>
   );
 }
--- src/components/UniForm/UniForm.jsx
+++ src/components/UniForm/UniForm.jsx
@@ -1,90 +1,56 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 
-function UniForm({ id, data, handleNewData }) {
-    const [car, setCar] = useState(data);
-    useEffect(() => {
-        setCar(data);
-    }, [data]);
+function UniForm({ id, data, handleNewData, handleClick }) {
 
     const handleChange = (e) => {
+        let temp = { ...data }
         const { name, value } = e.target;
-
-        let updatedCar = { ...car };
-
         switch (name) {
             case 'brand':
-                updatedCar.brand = value;
+                temp.brand = value;
                 break;
             case 'model':
-                updatedCar.model = value;
+                temp.model = value;
                 break;
             case 'reg':
-                updatedCar.reg = value;
+                temp.reg = value;
                 break;
             case 'km':
-                updatedCar.km = parseInt(value) || 0;
+                temp.km = parseInt(value) || 0;
                 break;
             case 'year':
-                updatedCar.year = parseInt(value) || 0;
+                temp.year = parseInt(value) || 0;
                 break;
             default:
                 break;
         }
-
-        setCar(updatedCar);
+        handleNewData(temp, id);
     };
-
-
-    const handleClick = () => {
-        const updatedCar = {
-            ...car,
-            brand: car.brand.trim() ? car.brand : "empty",
-            model: car.model.trim() ? car.model : "empty",
-            reg: car.reg.trim() ? car.reg : "empty",
-            km: car.km.toString().trim() ? parseInt(car.km) : 0,
-            year: car.year.toString().trim() ? parseInt(car.year) : 0,
-        };
-
-        const isConfirmed = window.confirm("Opravdu chcete odeslat data?\n" +
-            `Značka: ${updatedCar.brand}\n` +
-            `Model: ${updatedCar.model}\n` +
-            `Registrační značka: ${updatedCar.reg}\n` +
-            `Kilometry: ${updatedCar.km}\n` +
-            `Rok výroby: ${updatedCar.year}`);
-
-        if (isConfirmed) {
-            handleNewData(updatedCar, id);
-            alert("Data byla úspěšně odeslána.");
-        } else {
-            alert("Odeslání dat bylo zrušeno.");
-        }
-    };
-
 
     return (
         <div id={id}>
             <div>
                 <label htmlFor='brand'>Značka</label>
-                <input type='text' name='brand' value={car.brand} onChange={handleChange} />
+                <input type='text' name='brand' value={data.brand} onChange={handleChange} />
             </div>
             <div>
                 <label htmlFor='brand'>Model</label>
-                <input type='text' name='model' value={car.model} onChange={handleChange} />
+                <input type='text' name='model' value={data.model} onChange={handleChange} />
             </div>
             <div>
                 <label htmlFor='brand'>Registrační zn.</label>
-                <input type='text' name='reg' value={car.reg} onChange={handleChange} />
+                <input type='text' name='reg' value={data.reg} onChange={handleChange} />
             </div>
             <div>
                 <label htmlFor='brand'>Najeto km</label>
-                <input type='number' name='km' value={car.km} onChange={handleChange} />
+                <input type='number' name='km' value={data.km} onChange={handleChange} />
             </div>
             <div>
                 <label htmlFor='brand'>Rok výroby</label>
-                <input type='number' name='year' value={car.year} onChange={handleChange} />
+                <input type='number' name='year' value={data.year} onChange={handleChange} />
             </div>
             <div>
-                <button onClick={handleClick}>Odešli data</button>
+                <button onClick={() => handleClick(id)}>Odešli data</button>
             </div>
         </div>
     );
